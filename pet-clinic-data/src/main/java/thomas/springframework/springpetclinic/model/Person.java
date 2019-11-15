f906b4401049cd55f350b3505be893e50aca1583
ThomasDroege <ThomasDroege@gmx.de>
2019-11-15 23:34:09 +0100

    adding base entity. closes #1- 0

--- pet-clinic-data/src/main/java/thomas/springframework/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/thomas/springframework/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package thomas.springframework.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
