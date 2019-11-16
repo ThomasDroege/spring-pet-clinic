705f93f725d6276b05e5bfad7e8adc26d54ddbd5
ThomasDroege <ThomasDroege@gmx.de>
2019-11-16 13:13:11 +0100

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/thomas/springframework/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/thomas/springframework/springpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package thomas.springframework.springpetclinic.services;
 
 import thomas.springframework.springpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
