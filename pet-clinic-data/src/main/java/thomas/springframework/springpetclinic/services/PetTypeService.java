1876bf0820372d00c6dcc84aa71845b54ff6f9cb
ThomasDroege <ThomasDroege@gmx.de>
2019-11-17 21:09:19 +0100

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/thomas/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package thomas.springframework.springpetclinic.services;
+
+import thomas.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
