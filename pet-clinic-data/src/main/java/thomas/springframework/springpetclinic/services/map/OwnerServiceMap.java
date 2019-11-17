dd1e59d42b7f8f8a93c5442a61ed160e22bc37a9
ThomasDroege <ThomasDroege@gmx.de>
2019-11-17 22:09:33 +0100

    added Pets and Contact info to owners on startup. Closes #33

--- pet-clinic-data/src/main/java/thomas/springframework/springpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/thomas/springframework/springpetclinic/services/map/OwnerServiceMap.java
@@ -2,12 +2,23 @@ package thomas.springframework.springpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
 import thomas.springframework.springpetclinic.model.Owner;
+import thomas.springframework.springpetclinic.model.Pet;
 import thomas.springframework.springpetclinic.services.OwnerService;
+import thomas.springframework.springpetclinic.services.PetService;
+import thomas.springframework.springpetclinic.services.PetTypeService;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -21,7 +32,28 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+
+        if(object != null){
+            if(object.getPets() != null){
+                object.getPets().forEach(pet -> {
+                    if(pet.getPetType() != null){
+                        if(pet.getPetType().getId() == null){
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    }else{
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if(pet.getId() == null){
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        }else {
+            return null;
+        }
     }
 
     @Override
