ae83b0fe67e3ac27d315740b1c45ffe02d625e2d
ThomasDroege <ThomasDroege@gmx.de>
2019-11-28 21:20:01 +0100

    Implemented Map Impl for visit service. Closes #46

---
+++ pet-clinic-data/src/main/java/thomas/springframework/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package thomas.springframework.springpetclinic.services;
+
+import thomas.springframework.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
